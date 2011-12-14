49cf79b6fb464d355d31c515ce26a0d6881533a6
Steven Haryanto <stevenharyanto@gmail.com>
2011-12-14 07:17:34 +0000

    Import of SHARYANTO/Calendar-Indonesia-Holiday-0.07 from CPAN.
    gitpan-cpan-distribution: Calendar-Indonesia-Holiday
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         SHARYANTO/Calendar-Indonesia-Holiday-0.07.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Calendar/ID/Holiday.pm
+++ lib/Calendar/ID/Holiday.pm
@@ -1,6 +1,6 @@
 package Calendar::ID::Holiday;
 BEGIN {
-  $Calendar::ID::Holiday::VERSION = '0.06';
+  $Calendar::ID::Holiday::VERSION = '0.07';
 }
 use Calendar::Indonesia::Holiday;
 our @ISA       = @Calendar::Indonesia::Holiday::ISA;
@@ -23,7 +23,7 @@ Calendar::ID::Holiday - Alias for Calendar::Indonesia::Holiday
 
 =head1 VERSION
 
-version 0.06
+version 0.07
 
 =head1 AUTHOR
 
